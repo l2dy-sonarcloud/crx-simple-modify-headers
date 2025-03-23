76ff9b7dfc384f701db784ae1b2f25c903207041
Warren R. Bank <warren.r.bank@gmail.com>
2025-03-22 21:34:18 -0700

    revise methodology to determine when to display alternate popup icon
    Turns out that the color of the top menu bar in Firefox
    isn't dependent upon dark vs. light mode..
    only on whether the window is normal vs. private

--- simple-modify-headers/icon.js
+++ simple-modify-headers/icon.js
@@ -1,20 +1,21 @@
+// Firefox only
+
 function onTheme(isDark) {
   const color = isDark ? 'white' : 'black'
   chrome.runtime.sendMessage({action: 'change-default-icon', color})
 }
 
 function initTheme() {
-  const isFirefox = window.navigator.userAgent.toLowerCase().includes('firefox')
   const isPrivate = chrome.extension.inIncognitoContext
-
-  if (isFirefox && isPrivate) {
-    onTheme(true)
-  }
-  else if (window.matchMedia) {
-    const prefersDark = window.matchMedia('(prefers-color-scheme: dark)')
-    onTheme(prefersDark && prefersDark.matches)
-  }
+  onTheme(isPrivate)
 }
 
-window.addEventListener('focus', initTheme)
-initTheme()
+function initFirefox() {
+  const isFirefox = window.navigator.userAgent.toLowerCase().includes('firefox')
+  if (!isFirefox) return
+
+  window.addEventListener('focus', initTheme)
+  initTheme()  
+}
+
+initFirefox()
